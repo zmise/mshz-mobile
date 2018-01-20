249989f79ad64e3c8f0f75da30e9e0e5e33fa413
zmise <zmise@zmises-iMac.local>
2018-01-20 10:52:33 +0800

    首页轮播图的修正

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -15,23 +15,26 @@ require('../../assets/js/appDownload.js');//全局下载APP
 
 $(function () {
 
-  $('.house-base-info .base-server .icon-items:gt(4)').addClass('hide');
+  // $('.house-base-info .base-server .icon-items:gt(4)').addClass('hide');
   /* 焦点图片  */
   $('.banner-body').banner({
     'width': $(window).width(),
     'height': $(window).width() * 2 / 3,
-    'paginationType': 'fraction'
+    'paginationType': 'fraction',
+    'autoPlay': true, // 是否自动播放
+    'speed': 3000, // 播放速度
+
   });
 
-  /* switch header */
-  function switchHeader() {
-    var _hig = $('.banner-body').height();
-    if ($(document).scrollTop() > _hig) {
-      $('.header-body').show();
-    } else {
-      $('.header-body').hide();
-    }
-  };
+  // /* switch header */
+  // function switchHeader() {
+  //   var _hig = $('.banner-body').height();
+  //   if ($(document).scrollTop() > _hig) {
+  //     $('.header-body').show();
+  //   } else {
+  //     $('.header-body').hide();
+  //   }
+  // };
 
   /* switch header   */
   var timeoutObject;
