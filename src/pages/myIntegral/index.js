ad24eaefc75030680dec42e9a7a4244044d21245
zmise <zmise@zmises-iMac.local>
2018-01-22 10:16:06 +0800

    tobelandlord页面的首尾结构和personl-center页面的跳转逻辑

--- src/pages/myIntegral/index.js
+++ src/pages/myIntegral/index.js
@@ -4,6 +4,16 @@ require('./index.scss');
 require('../../assets/js/plugins.js');
 require('../../assets/js/navigate.js');
 require('../../assets/js/appDownload.js');//全局下载APP
+$(function () {
+
+
+  // 点击返回回到上一页
+  $('#back').on('tap', function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+    history.go(-1)
+  });
+});
 
 
 
--- src/pages/personal-center/index.js
+++ src/pages/personal-center/index.js
@@ -6,3 +6,39 @@ require('../../assets/js/navigate.js');
 require('../../assets/js/appDownload.js');//全局下载APP
 
 
+$(function () {
+
+  //点击进入个人中心
+  $('.article-body').on('tap', '#myInegral-entry', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    window.location = './myIntegral.html';
+  });
+  //点击进入订单列表
+  $('.article-body').on('tap', '#orderList-entry', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    window.location = './order-list.html';
+
+  });
+
+  //点击进入成为房东页面
+  $('.article-body').on('tap', '#landlord-entry', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    window.location = './tobelandlord.html';
+  });
+  //点击进入成为房东页面
+  $('.article-body').on('tap', '#landlord-entry', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    window.location = './tobelandlord.html';
+  });
+
+  // 点击返回回到上一页
+  $('#back').on('tap', function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+    history.go(-1)
+  });
+});
