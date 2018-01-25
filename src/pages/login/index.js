99bc57af072875a67001a6ab5523aaaf7d349b1c
zmise <zmise@zmises-iMac.local>
2018-01-25 18:21:44 +0800

    登录模块的ui（修改密码，手机登录，用户登录）

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,35 @@
 require('./index.scss');
+// require('../../assets/js/plugins.js');
+
+require('../../assets/js/toast.js');  //toast的事件
 
 
 $(function () {
+
+  // 初始化的弹出的toast框
+  function showMessage(content, duration, isCenter, animateIn, animateOut) {
+    var animateIn = animateIn;
+    var animateOut = animateOut;
+    var content = content;
+    var duration = duration;
+    var isCenter = isCenter;
+    $('body').toast({
+      position: 'fixed',
+      animateIn: animateIn,
+      animateOut: animateOut,
+      content: content,
+      duration: duration,
+      isCenter: isCenter,
+      padding: '0.1rem 0.27rem',
+      background: 'rgba(181, 185, 190, 0.8)',
+      borderRadius: '.31rem',
+      fontSize: '.24rem',
+      top: "80%",        //bottom底部的位置    具体的数值 或者center  垂直居中
+    });
+  }
+  // showMessage('zmise', 100000, true, 'bounceInUp-hastrans', 'bounceOutDown-hastrans');
+  showMessage('这是提示消息哈！');
+
   // 点击返回回到上一页
   $('#back').on('click', function (e) {
 
