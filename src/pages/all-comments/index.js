5cedbb818161b7c8f5b91c610bacc5324ff9a018
Zhongmin <531625687@qq.com>
2018-01-31 11:21:10 +0800

    all-comments的交互完成

--- src/pages/all-comments/index.js
+++ src/pages/all-comments/index.js
@@ -7,7 +7,7 @@ require('../../assets/js/appDownload.js');//全局下载APP
 $(function () {
 
   // 房源评价相关数据get接口
-  function guessLikeInfo(params) {
+  function queryRoomComment(params) {
     $.ajax({
       url: '/mshz-app/order/comment/queryRoomComment',
       data: params,
@@ -17,66 +17,77 @@ $(function () {
       success: function (res) {
         if (res.status === 'C0000') {
           //todo
+          console.log(res);
           var data = res.result;
           var str = '';
           str =
             '<div class="total">' +
             '  <div class="line-two">' +
             '    <div class="flex-box">' +
-            '      <i class="clc-red txt socre">4.7分</i>' +
+            '      <i class="clc-red txt socre">' + data.rate + '分</i>' +
             '      <div class="star-lines"></div>' +
             '    </div>' +
             '    <div>' +
-            '      <i class="clc-red txt" href="javascript:;">环境：4.7</i>' +
-            '      <i class="clc-red txt" href="javascript:;">服务：4.8</i>' +
+            '      <i class="clc-red txt" href="javascript:;">环境：' + data.rateEnv + '</i>' +
+            '      <i class="clc-red txt" href="javascript:;">服务：' + data.rateServer + '</i>' +
             '    </div>' +
             '  </div>' +
             '</div>';
-          var array = data.items;
+          var array = data.comment.items || [];
           for (var i = 0; i < array.length; i++) {
             str +=
               '<div class="comment-body">' +
               '  <div class="first-comment">' +
               '    <div class="info">' +
               '      <div class="flex-box">' +
-              '        <div class="photo"></div>' +
+              '        <img class="photo" src="' + array[i].headPortrait.replace('{size}', '88x88') + '"/>' +
               '        <div class="name-time">' +
-              '          <span>云来***9</span>' +
-              '          <span>2017-05-08</span>' +
+              '          <span>' + array[i].nickName + '</span>' +
+              '          <span>' + array[i].commentTimeDesc + '</span>' +
               '        </div>' +
               '      </div>' +
-              '      <span class="socre">5.0分</span>' +
+              '      <span class="socre">' + array[i].score + '分</span>' +
               '    </div>' +
               '    <div class="des">' +
-              '      <p class="des-cnt">第一交通非常便利，离雍和宫地铁站仅200米，二四合院气氛非常浓，。</p>' +
-              '      <div class="img-list">' +
-              '        <img class="items " src="./img/feature-garden.jpg"></img>' +
-              '        <img class="items"></img>' +
-              '        <img class="items"></img>' +
-              '        <img class="items"></img>' +
-              '      </div>' +
-              '      <div class="des-duihua">';
+              '      <p class="des-cnt">' + array[i].content + '</p>';
 
-            var list = array[i].replyList;
-            for (var j = 0; j < array.length; j++) {
-
-              str +=
-                '        <div class="items">' +
-                '          <span>' +
-                '            <i class="current">管家</i>回复：谢谢你的点评，欢迎下次入sdfdsfdfsdf住。</span>' +
-                '        </div>'
-                ;
+            if (array[i].commentPicture !== '') {
+              str += '      <div class="img-list">';
+              var comPicArray = array[i].commentPicture.split(',');
+              for (var k = 0; k < comPicArray.length; k++) {
+                str +=
+                  '        <img class="items " src="' + comPicArray[k].replace('{size}', '144x144') + '"/>';
+              }
+              str += '      </div>';
             }
 
+            var list = array[i].replyList;
+            if (list.length > 0) {
+              str += '      <div class="des-duihua">'
+              for (var j = 0; j < list.length; j++) {
+                str +=
+                  '        <div class="items">' +
+                  '          <span>';
+
+                if (list[i].replyPersonType === 'MANAGER') {
+                  str += '            <i class="current">管家</i>回复：' + list[i].replyContent + '</span>';
+
+                } else {
+                  str += '            <i class="current">' + list[i].replyName + '</i>回复：' + list[i].replyContent + '</span>';
+                }
+
+                str += '        </div>';
+              }
+              str += '      </div>';
+            }
             str +=
-              '      </div>' +
               '    </div>' +
               '  </div>' +
               '</div>';
 
           }
 
-
+          $('#articleBody').append(str);
         }
       },
       error: function (error) {
@@ -102,14 +113,15 @@ $(function () {
     // 关闭loading
     $('#loading').remove();
     $('#roomId').val(roomId);
+    var params = {
+      roomId: roomId,
+    }
+    queryRoomComment(params)
   }
 
-  var params = {
-    roomId: roomId,
-  }
 
 
-  guessLikeInfo();
+
   // 点击返回回到上一页
   $('#back').on('tap', function (e) {
     e.stopPropagation();
