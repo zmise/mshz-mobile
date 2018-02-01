959c18c65481e4db43aa45435fbbdec836245f0a
Zhongmin <531625687@qq.com>
2018-02-01 17:53:13 +0800

    bugfix #55131【个人中心】姓名设置后，返回乱码

--- src/pages/change-nickname/index.js
+++ src/pages/change-nickname/index.js
@@ -61,7 +61,8 @@ $(function () {
   function getUrlParam(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
     var r = window.location.search.substr(1).match(reg);  //匹配目标参数
-    if (r != null) return unescape(r[2]); return null; //返回参数值
+    if (r != null) return decodeURI(r[2]); //处理中文乱码
+    return null; //返回参数值
   }
 
   var nickname = getUrlParam('nickname');
@@ -79,7 +80,7 @@ $(function () {
     e.stopPropagation();
     e.preventDefault();
     var nickname = $.trim($('#nickname').val());
-    if (!reg.test(nickname)) {
+    if (nickname === '') {
       showMessage('请输入昵称！');
       return;
     }
