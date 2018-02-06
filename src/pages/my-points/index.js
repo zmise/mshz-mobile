3b899eb7e06247975b9f9426301cd0c8ea09d31a
Zhongmin <531625687@qq.com>
2018-02-06 18:32:41 +0800

    my-points页面的接口联调

--- src/pages/my-points/index.js
+++ src/pages/my-points/index.js
@@ -7,7 +7,41 @@ require('../../assets/js/navigate.js');
 require('../../assets/js/appDownload.js');//全局下载APP
 $(function () {
 
-
+  //queryUserPointsRecord get接口
+  $.ajax({
+    url: '/mshz-app/security/user/queryUserPointsRecord',
+    dataType: 'json',
+    type: 'GET',
+    cache: false,
+    success: function (res) {
+      if (res.status === 'C0000') {
+        console.log(res);
+        var item = res.result.items;
+        var str = '';
+        var score = 0;
+        for (var i = 0; i < item.length; i++) {
+          str +=
+            '<div class="items">' +
+            '  <div class="txt">' +
+            '    <span class="theme">' + item[i].title + '</span>' +
+            '    <span class="time">' + item[i].usage + ' ' + item[i].createTimeDesc
+            + '</span>' +
+            '  </div>' +
+            '  <span class="score">+' + item[i].points + '</span>' +
+            '</div>'
+            ;
+          score += parseInt(item[i].points);
+        }
+        $('#list').append(str);
+        $('#score').text(score);
+      }
+    },
+    error: function (error) {
+      console.log(error);
+      console.log('error');
+    }
+  });
+  // 
   // 点击返回回到上一页
   $('#back').on('tap', function (e) {
     e.stopPropagation();
