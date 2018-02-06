871ebeac026e91b8258a859e805ae31c2153c25c
Zhongmin <531625687@qq.com>
2018-02-06 17:38:26 +0800

    about-us页面结构和调整

--- src/pages/about-us/index.js
+++ src/pages/about-us/index.js
@@ -2,3 +2,11 @@ require('./index.scss');
 
 require('../../assets/js/plugins.js');
 require('../../assets/js/navigate.js');
+
+
+$(function () {
+  var loginInfo = JSON.parse(window.sessionStorage.getItem('loginInfo'));
+  if (loginInfo) {
+    $('#menu').removeClass('icon-gerenzhongxin').addClass('icon-daohanglancaidan').attr('href', 'javascript:;');
+  }
+});
