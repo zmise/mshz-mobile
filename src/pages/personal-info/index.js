994a38c6099882c3e891970f884a6b42775a4bae
Zhongmin <531625687@qq.com>
2018-02-06 12:35:32 +0800

    bugfix #55253 【个人中心】个人中心头像上传失败

--- src/pages/personal-info/index.js
+++ src/pages/personal-info/index.js
@@ -34,49 +34,80 @@ $(function () {
     });
   }
 
-  //从本地上传图片文件(input type=file)
-  function imgPreview(fileDom) {
-    //判断是否支持FileReader
-    if (window.FileReader) {
-      var reader = new FileReader();
-    } else {
-      alert("您的设备不支持图片预览功能，如需该功能请升级您的设备！");
-    }
 
-    //获取文件
-    // console.log(fileDom);
+  //上传本地图片转换为URL post接口
+  function uploadImage(file) {
+    console.log('zmise')
+    var dtd = $.Deferred();
+    var params = new FormData();
+    params.append('file', file, file.name);
+    $.ajax({
+      url: '/mshz-app/security/user/updateLoadPicture',
+      type: 'post',
+      contentType: false,
+      processData: false,
+      data: params
+    }).then(function (res) {
+      if (res.status === 'C0000') {
 
-    var file = fileDom[0].files[0];
-    // console.log(file);
-
-    var imageType = /^image\//;
-    //是否是图片
-    if (!imageType.test(file.type)) {
-      alert("请选择图片！");
-      return;
-    }
-    //读取完成
-    reader.onload = function (e) {
-      //获取图片dom
-      // console.log(e.target);
-      // console.log(e.target.result);
-      // var $img = $('#img');
-      // //图片路径设置为读取的图片
-      // $img.attr('src', e.target.result);
-      // console.log(fileDom.next());
-      if (e.target.result) {
-        $('#image').attr('src', e.target.result);
+        console.log(res.result.url);
+        $('#image').attr('src', res.result.url.replace('{size}', '88x88'));
         var params = {
-          headPortrait: e.target.result,
+          headPortrait: res.result.url,
         }
         updateUserInfo(params);
       } else {
-        showMessage('传入的图片文件不正确');
+        dtd.reject(new Error(res.message));
       }
-
-    };
-    reader.readAsDataURL(file);
+    }).fail(function (err) {
+      dtd.reject(new Error('上传失败，请检查网络。'));
+    });
+    return dtd.promise();
   }
+
+  // //从本地上传图片文件(input type=file)
+  // function imgPreview(fileDom) {
+  //   //判断是否支持FileReader
+  //   if (window.FileReader) {
+  //     var reader = new FileReader();
+  //   } else {
+  //     alert("您的设备不支持图片预览功能，如需该功能请升级您的设备！");
+  //   }
+
+  //   //获取文件
+  //   // console.log(fileDom);
+
+  //   var file = fileDom[0].files[0];
+  //   // console.log(file);
+
+  //   var imageType = /^image\//;
+  //   //是否是图片
+  //   if (!imageType.test(file.type)) {
+  //     alert("请选择图片！");
+  //     return;
+  //   }
+  //   //读取完成
+  //   reader.onload = function (e) {
+  //     //获取图片dom
+  //     // console.log(e.target);
+  //     // console.log(e.target.result);
+  //     // var $img = $('#img');
+  //     // //图片路径设置为读取的图片
+  //     // $img.attr('src', e.target.result);
+  //     // console.log(fileDom.next());
+  //     if (e.target.result) {
+  //       $('#image').attr('src', e.target.result);
+  //       var params = {
+  //         headPortrait: e.target.result,
+  //       }
+  //       updateUserInfo(params);
+  //     } else {
+  //       showMessage('传入的图片文件不正确');
+  //     }
+
+  //   };
+  //   reader.readAsDataURL(file);
+  // }
 
   //用户个人中心所需数据get接口
   function queryInfo() {
@@ -190,6 +221,7 @@ $(function () {
     });
   }
 
+
   queryInfo();
 
 
@@ -198,13 +230,13 @@ $(function () {
   $('.article-body').on('click', '#image', function (e) {
     // e.stopPropagation();
     // e.preventDefault();
-    console.log(132);
     $('#img-file').click();
   });
   $('.article-body').on('change', '#img-file', function (e) {
-    // e.stopPropagation();
-    // e.preventDefault();
-    imgPreview($(this));
+    e.stopPropagation();
+    e.preventDefault();
+    var file = this.files[0];
+    uploadImage(file);
   });
 
 
