4dd1ff1e140aab47721f0c6a8c4c32f0842c99a1
Zhongmin <531625687@qq.com>
2018-02-07 16:42:06 +0800

    tobelandlord页面的布局和交互

--- src/pages/tobelandlord/index.js
+++ src/pages/tobelandlord/index.js
@@ -2,7 +2,6 @@ require('./index.scss');
 
 /* 侧边导航 */
 require('../../assets/js/plugins.js');
-require('../../assets/js/navigate.js');
 
 require('../../assets/js/appDownload.js');//全局下载APP
 
@@ -10,7 +9,12 @@ require('../../assets/js/appDownload.js');//全局下载APP
 
 $(function () {
 
-
+  $('#btn').on('tap', function (e) {
+    event.stopPropagation();
+    event.preventDefault();
+    window.location = 'tel:42032002800';
+    $('#overlay').hide();
+  });
   // 点击返回回到上一页
   $('#back').on('tap', function (e) {
     e.stopPropagation();
