a07ca33a037664b703806e0438c5b5ab7e0b5f9d
Zhongmin <531625687@qq.com>
2018-02-07 21:26:26 +0800

    bugfix #54665 【APP推荐】关闭APP推荐，刷新页面，推荐广告还存在

--- src/assets/js/appDownload.js
+++ src/assets/js/appDownload.js
@@ -3,4 +3,8 @@ $('.app-download').on('tap', '.close', function (e) {
   e.stopPropagation();
   e.preventDefault();
   $('.app-download').css('display', 'none');
+  window.sessionStorage.setItem('closeApp', true);
 });
+if (window.sessionStorage.getItem('closeApp')) {
+  $('.app-download').css('display', 'none');
+}
