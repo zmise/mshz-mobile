cfffabd9beba52c5cf11fbe8157c19123bce57a6
Zhongmin <531625687@qq.com>
2018-04-17 16:20:36 +0800

    解决appDown下载提示在vivo上删除不了的问题

--- src/assets/js/appDownload.js
+++ src/assets/js/appDownload.js
@@ -4,8 +4,8 @@ $('.app-download').on('tap', '.close', function (e) {
   e.stopPropagation();
   e.preventDefault();
   $('.app-download').css('display', 'none');
-  window.sessionStorage.setItem('closeApp', true);
+  window.localStorage.setItem('closeApp', true);
 });
-if (window.sessionStorage.getItem('closeApp')) {
+if (window.localStorage.getItem('closeApp')) {
   $('.app-download').css('display', 'none');
 }
